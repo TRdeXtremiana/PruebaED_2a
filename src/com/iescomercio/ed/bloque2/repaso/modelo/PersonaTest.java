e614a61d079c3de39799c6a899652fe01212e2b5
diego extremiana <johndoe@example.com~dextremiana1998@gmail.com~>
2022-02-28 13:06:08 +0100

    testPersona

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/PersonaTest.java
@@ -0,0 +1,47 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void testPersona() {
+		Persona p = new Persona("12345678A", "Juan", "Serrano");
+		assertEquals("12345678A", p.getDni());
+		assertEquals("Juan", p.getNombre());
+		assertEquals("Serrano", p.getApellido1());
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
