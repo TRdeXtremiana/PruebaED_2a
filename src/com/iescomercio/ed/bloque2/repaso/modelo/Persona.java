dc5fdebbf0b1296d1694adddcd2ab20e10af3a11
diego extremiana <johndoe@example.com~dextremiana1998@gmail.com~>
2022-02-28 23:07:30 +0100

    documentada <setApellido1>

--- src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
@@ -77,8 +77,11 @@ public class Persona {
 		return apellido1;
 	}
 
-	/*
-	 * Documentar y crear test unitario
+	/**
+	 * Nos permite modificar el apellido de una persona. Requiere el parametro
+	 * String de entrada del apellido que queremos cambiar
+	 * 
+	 * @param apellido1
 	 */
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
